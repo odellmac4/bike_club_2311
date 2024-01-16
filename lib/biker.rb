9fa200e4858e27b62376b2bfc62db513c48dd225
Odell McFarland IV <odell.mac4@gmail.com>
2024-01-16 08:51:33 -0800

    Feat: logs rides for biker with ride and its times

--- lib/biker.rb
+++ lib/biker.rb
@@ -5,11 +5,18 @@ class Biker
     def initialize(name , max_distance)
         @name = name
         @max_distance = max_distance
-        @rides = {}
+        @rides = Hash.new { |h_obj, k| h_obj[k] = [] }
         @acceptable_terrain = []
     end
 
     def learn_terrain!(terrain)
         @acceptable_terrain << terrain
+    end
+
+    def log_ride(ride , time)
+        if @acceptable_terrain.include?(ride.terrain) && ride.total_distance < @max_distance
+            @rides[ride] << time
+        end
+        @rides
     end
 end
