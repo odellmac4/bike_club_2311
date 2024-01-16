314217bc5048e0efaccec9dfd4d8f602b1123d97
Odell McFarland IV <odell.mac4@gmail.com>
2024-01-16 10:59:56 -0800

    Feat: returns an array of eligible bikers for a given ride

--- lib/bike_club.rb
+++ lib/bike_club.rb
@@ -10,4 +10,22 @@ class BikeClub
     def add_biker(biker)
         @bikers << biker
     end
+
+    def most_rides
+        @bikers.max_by do |biker|
+            biker.rides.count
+        end
+    end
+
+    def best_time(ride)
+        @bikers.max_by do |biker|
+            biker.rides[ride]
+        end
+    end
+    
+    def bikers_eligible(ride)
+        @bikers.select do |biker|
+            biker.can_ride?(ride)
+        end
+    end
 end
