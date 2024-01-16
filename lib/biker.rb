fecf3a2a801b700921a2262e51279cc301796ef8
Odell McFarland IV <odell.mac4@gmail.com>
2024-01-16 08:26:57 -0800

    Feat: biker can learn terrains

--- lib/biker.rb
+++ lib/biker.rb
@@ -1,9 +1,15 @@
 class Biker
 
-    attr_reader :name, :max_distance
+    attr_reader :name, :max_distance, :rides, :acceptable_terrain
 
     def initialize(name , max_distance)
         @name = name
         @max_distance = max_distance
+        @rides = {}
+        @acceptable_terrain = []
+    end
+
+    def learn_terrain!(terrain)
+        @acceptable_terrain << terrain
     end
 end
