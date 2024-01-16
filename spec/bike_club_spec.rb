b7a5c2d8cbd202c01dea5e0ac0ee15b86768ce18
Odell McFarland IV <odell.mac4@gmail.com>
2024-01-16 09:52:27 -0800

    Test: bike club can add bikers

--- spec/bike_club_spec.rb
+++ spec/bike_club_spec.rb
@@ -7,20 +7,32 @@ RSpec.describe BikeClub do
       @ride1 = Ride.new({name: "Walnut Creek Trail", distance: 10.7, loop: false, terrain: :hills})
       @ride2 = Ride.new({name: "Town Lake", distance: 14.9, loop: true, terrain: :gravel})
 
-      @bikeclub = BikeClub.new("Wild Hogs")
+      @biker1 = Biker.new("Kenny", 30)
+      @biker2 = Biker.new("Athena", 15)
+
+      @bike_club = BikeClub.new("Wild Hogs")
     end
   
     describe 'bike club' do
       it 'is a bike club' do
-        expect(@bikeclub).to be_a BikeClub
+        expect(@bike_club).to be_a BikeClub
       end
   
       it 'has a name' do
-        expect(@bikeclub.name).to eq("Wild Hogs")
+        expect(@bike_club.name).to eq("Wild Hogs")
       end
 
       it 'has bikers' do
-        expect(@bikeclub.bikers).to eq ([])
+        expect(@bike_club.bikers).to eq ([])
       end
+    end
+
+    describe '#add_biker' do
+        it 'adds bikers' do
+            @bike_club.add_biker(@biker1)
+            @bike_club.add_biker(@biker2)
+            
+            expect(@bike_club.bikers).to eq ([@biker1 , @biker2])
+        end
     end
 end
