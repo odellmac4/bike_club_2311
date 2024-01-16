1f3dce0378b0770bd716a41b9ee4a145e6eac4f6
Odell McFarland IV <odell.mac4@gmail.com>
2024-01-16 08:52:14 -0800

    Test: return has with ride and bikers times in an array

--- spec/biker_spec.rb
+++ spec/biker_spec.rb
@@ -4,6 +4,10 @@ require './lib/biker'
 RSpec.describe Biker do
     before(:each) do
       @biker = Biker.new("Kenny", 30)
+
+      @ride1 = Ride.new({name: "Walnut Creek Trail", distance: 10.7, loop: false, terrain: :hills})
+
+      @ride2 = Ride.new({name: "Town Lake", distance: 14.9, loop: true, terrain: :gravel})
     end
   
     describe 'biker' do
@@ -33,6 +37,29 @@ RSpec.describe Biker do
             @biker.learn_terrain!(:gravel)
             @biker.learn_terrain!(:hills)
             expect(@biker.acceptable_terrain).to eq ([:gravel, :hills])
+        end
+    end
+
+    describe '#log_ride' do
+        it 'logs previous rides and times for those rides. will not log if the rides terrain isnt acceptable and the rides total distance is greater than biker max distance.' do
+            @biker.learn_terrain!(:gravel)
+            @biker.learn_terrain!(:hills)
+            
+            @biker.log_ride(@ride1, 92.5)
+            @biker.log_ride(@ride1, 91.1)
+            @biker.log_ride(@ride2, 60.9)
+            @biker.log_ride(@ride2, 61.6)
+
+            expect(@biker.rides).to eq ({
+                @ride1 => [92.5, 91.1],
+                @ride2 => [60.9, 61.6]
+            })
+        end
+    end
+
+    describe '#personal_record' do
+        it 'report personal record for a specific ride. The lowest time recorded for a ride. False if ride not completed' do
+            
         end
     end
 end
