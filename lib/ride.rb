148542606ecf33f9eddd17ff8949ed7ee8f58d8e
Odell McFarland IV <odell.mac4@gmail.com>
2024-01-16 08:07:53 -0800

    Feat: initialize a ride

--- lib/ride.rb
+++ lib/ride.rb
@@ -1,3 +1,10 @@
 class Ride
 
+    attr_reader :name, :distance
+
+    def initialize(info)
+        @name = info[:name]
+        @distance = info[:distance]
+        @loop = info[:loop]
+    end
 end
